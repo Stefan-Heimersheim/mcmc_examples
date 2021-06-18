6a897f39096f5f60fe6cbd6b8ca30729d6cfffc9
Stefan Heimersheim <heimersheim@ast.cam.ac.uk>
2021-06-18 15:36:24 +0100

    Solution

--- 1b_own_mcmc.py
+++ 1b_own_mcmc.py
@@ -7,7 +7,6 @@ def likelihood(p):
 
 def prior(p):
     return np.product(sst.uniform.pdf(np.array(p), loc=-5, scale=10), axis=0)
-    # -5 to 5
 
 posterior = lambda p: likelihood(p)*prior(p)
 # (unnormalized)
@@ -37,17 +36,34 @@ plt.show()
 nDims = 2
 
 # Starting points
-mcmc_points = [ [-5,-5] ]
-posterior_values = [ posterior(mcmc_points[0]) ]
+mcmc_points = [np.array([-5]*nDims)]
+posterior_values = [posterior(mcmc_points[0])]
+
+scale = 1
+accepted_count = 0
+rejected_count = 0
 
 
-
-for i in range(100):
+imax = 10000
+for i in range(imax):
+    if i%10000 == 0:
+        print(i, imax)
     old_point = mcmc_points[-1]
     old_posterior = posterior_values[-1]
-    # generate new samples
-    # accept/reject them
-    # append new/old sample to mcmc_points and (optionally) posterior_values
+    new_point = old_point + np.random.normal(scale=[scale]*nDims)
+    new_posterior = posterior(new_point)
+    a = np.random.uniform()
+    if a < new_posterior/old_posterior:
+        accepted_count += 1
+        #print("Accepted :)")
+        mcmc_points.append(new_point)
+        posterior_values.append(new_posterior)
+    else:
+        #print("Rejected :(")
+        rejected_count += 1
+        mcmc_points.append(old_point)
+        posterior_values.append(old_posterior)
+
 
 P = np.array(mcmc_points)
 plt.hist2d(P.T[0], P.T[1], bins=100)
