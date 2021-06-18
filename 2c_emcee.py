4636890b0736eca5b858306539e9d75a7724e02f
Stefan Heimersheim <heimersheim@ast.cam.ac.uk>
2021-06-18 13:43:55 +0100

    Credit to emcee

--- 2c_emcee.py
+++ 2c_emcee.py
@@ -33,8 +33,8 @@ emcee_sampler = emcee.EnsembleSampler(nWalkers, nDim, logposterior_function)
 
 
 # Check if chain is converged using autocorrelation length.
-# from here: https://emcee.readthedocs.io/en/stable/user/autocorr/
-
+# Autocorrelation code is taken from emcee (MIT license) documentation, see below for details.
+# From here: https://emcee.readthedocs.io/en/stable/user/autocorr/
 
 # Run chain for 10000 points and track autocorrelation "tau"
 max_n = 10000
@@ -74,3 +74,29 @@ plt.xlabel("number of steps $N$")
 plt.ylabel(r"mean autocorrelation time $\hat{\tau}$");
 plt.legend()
 plt.show()
+
+
+
+"""
+The MIT License (MIT)
+
+Copyright (c) 2010-2019 Daniel Foreman-Mackey & contributors.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
